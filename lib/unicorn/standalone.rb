1bb22042bee0bb9a8af60029b00bbd987b5f1a5d
Noah Zoschke <nzoschke@gmail.com>
2013-09-10 13:47:30 -0700

    bundle gem skeleton

---
+++ lib/unicorn/standalone.rb
@@ -0,0 +1,7 @@
+require "unicorn/standalone/version"
+
+module Unicorn
+  module Standalone
+    # Your code goes here...
+  end
+end
---
+++ lib/unicorn/standalone/version.rb
@@ -0,0 +1,5 @@
+module Unicorn
+  module Standalone
+    VERSION = "0.0.1"
+  end
+end
